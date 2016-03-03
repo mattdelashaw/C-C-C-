2e3c1c6ea92f773283a75bc4e147a33acb72efcf
Matt Delashaw <mattdelashaw@gmail.com>
2016-03-02 22:50:44 -0600

    Create helloworld.cs
    using mono-project for mac
    mcs helloworld.cs
    mono helloworld.exe

---
+++ helloworld.cs
@@ -0,0 +1,10 @@
+using System;
+namespace HelloWorldApplication {
+	class HelloWorld {
+		static void Main(string[] args) {
+			/*comment*/
+			Console.WriteLine("Hello World \n");
+			Console.ReadKey();
+		}
+	}
+}
