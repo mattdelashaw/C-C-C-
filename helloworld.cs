f2fe43be15f9d7e0bfdad08d88fcac9a3fd46108
Matt Delashaw <mattdelashaw@gmail.com>
2016-03-08 19:44:13 -0600

    Update helloworld.cs

--- helloworld.cs
+++ helloworld.cs
@@ -2,7 +2,9 @@ using System;
 namespace HelloWorldApplication {
 	class HelloWorld {
 		static void Main(string[] args) {
-			/*comment*/
+			/*using mono-project for mac
+			mcs helloworld.cs
+			mono helloworld.exe*/
 			Console.WriteLine("Hello World \n");
 			Console.ReadKey();
 		}
